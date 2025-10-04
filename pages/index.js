f528f6ef926bbad107fbec3faf9759863606879b
cyberghost <149342849+GhoziTech@users.noreply.github.com>
2025-10-04 11:29:49 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import Head from "next/head";
 import Script from "next/script";
-import firebaseConfig, { APP_ID } from "../firebaseConfig";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      import("../public/osint-script.js")
+        .then(() => {
+          console.log("OSINT Script loaded");
+        })
+        .catch((e) => {
+          console.error("Failed to load script", e);
+        });
+    }
+  }, []);
+
   return (
     <>
       <Head>
@@ -12,18 +24,28 @@ export default function Home() {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.FIREBASE_CONFIG_JSON = \`${JSON.stringify(firebaseConfig)}\`;
-              window.APP_ID = "${APP_ID}";
-            `
+              window.FIREBASE_CONFIG_JSON = JSON.stringify(${JSON.stringify({
+                apiKey: "YOUR_API_KEY",
+                authDomain: "YOUR_PROJECT.firebaseapp.com",
+                projectId: "YOUR_PROJECT_ID",
+                storageBucket: "YOUR_PROJECT.appspot.com",
+                messagingSenderId: "SENDER_ID",
+                appId: "APP_ID",
+                measurementId: "MEASUREMENT_ID"
+              })});
+              window.APP_ID = "osint-v7";
+            `,
           }}
         />
       </Head>
 
       <Script src="https://cdn.tailwindcss.com" strategy="beforeInteractive" />
       <Script src="https://cdnjs.cloudflare.com/ajax/libs/tone/14.8.49/Tone.min.js" strategy="beforeInteractive" />
-      <Script type="module" src="/osint-script.js" strategy="afterInteractive" />
 
-      <main id="root" />
+      {/* Root of the original index.html layout (replaced with embedded) */}
+      <div id="root">
+        <noscript>Loading...</noscript>
+      </div>
     </>
   );
 }
